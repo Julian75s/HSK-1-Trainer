4304e488487fb6f2235cc4b3e7ccd34e60ec2b3f
Julian75s <50991957+Julian75s@users.noreply.github.com>
2026-07-27 22:55:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,7 @@ let score = 0;
 let isAnswered = false; // Bloque les clics multiples pendant le délai
 
 function shuffle(array) {
-  const arr = [...array];
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
-  return arr;
+  return array.sort(() => Math.random() - 0.5);
 }
 
 function startNewGame() {
